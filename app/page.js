07b4cadf72207baa7f5139f28244af1fb1e35704
Sarah Roperch <sarah.roperch@nexelec.fr>
2024-03-19 16:55:26 +0100

    first commit

---
+++ app/page.js
@@ -0,0 +1,38 @@
+import Link from "next/link";
+
+import ImageSlideshow from "@/components/images/images-slideshow";
+import classes from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <section className={classes.section}>
+          <h2>How it works</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+
+        <section className={classes.section}>
+          <h2>Why NextLevel Food?</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+      </main>
+    </>
+  );
+}
